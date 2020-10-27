5fc7a0a4e7a9cbabd70df14fee75cfd73462b77f
Dugald Steer <dugsteer@gmail.com>
2020-10-27 18:30:12 +0100

    Superhero version

--- script.js
+++ script.js
@@ -1,68 +1,69 @@
-const img1 = document.getElementById('mypic'); 
-const inputpanel = document.getElementById('inputpanel');
-const getButton = document.getElementById('getButton');
+const img1 = document.getElementById("mypic");
+const inputpanel = document.getElementById("inputpanel");
+const getButton = document.getElementById("getButton");
 
-
-
-window.addEventListener('load', function() {
-  document.querySelector('input[type="file"]').addEventListener('change', function() {
+window.addEventListener("load", function () {
+  document
+    .querySelector('input[type="file"]')
+    .addEventListener("change", function () {
       if (this.files && this.files[0]) {
-          img1.src = URL.createObjectURL(this.files[0]);
-          getButton.style.display = "none";
-          img1.style.display = "block";
-        
+        img1.src = URL.createObjectURL(this.files[0]);
+        getButton.style.display = "none";
+        img1.style.display = "block";
       }
-  });
+    });
 });
 
 const board = document.getElementById("board");
 const die = document.querySelector(".dice");
 const roll = document.getElementById("roll");
-const imgdiv = document.getElementById('imgdiv');
-const rolly = document.getElementById('rolly');
-const me = document.getElementById('me');
+const imgdiv = document.getElementById("imgdiv");
+const rolly = document.getElementById("rolly");
+const me = document.getElementById("me");
 
-function goforit(){
+function goforit() {
   imgdiv.innerHTML = ` <img src="img/${rolly.value}" alt="">`;
   rolly.style.display = "none";
   me.style.display = "none";
 }
 
-function flash(){
-  die.classList.add('flash');
+function flash() {
+  die.classList.add("flash");
 }
 
-function flashoff(){
-  die.classList.remove('flash');
+function flashoff() {
+  die.classList.remove("flash");
 }
 
 //rollDice
-function rollDice(){
-    // get a number between 0 and 5
-    let result= Math.floor(Math.random() * 6);
-    flash();
-    die.textContent="";
-    function setText(){
-      die.textContent = result + 1;
-    }
-    //set the dice text on the DOM to 1 to 6;
-    setTimeout(flashoff, 100);
-    setTimeout(setText, 200);
-};
+function rollDice() {
+  // get a number between 0 and 5
+  let result = Math.floor(Math.random() * 6);
+  flash();
+  die.textContent = "";
+  function setText() {
+    die.textContent = result + 1;
+  }
+  //set the dice text on the DOM to 1 to 6;
+  setTimeout(flashoff, 100);
+  setTimeout(setText, 200);
+}
 
-roll.addEventListener('click', rollDice);
-
+roll.addEventListener("click", rollDice);
 
 // Move pieces
 dragElement(document.getElementById("piece1"));
 dragElement(document.getElementById("piece2"));
-// dragElement(document.getElementById("piece3"));
-// dragElement(document.getElementById("piece4"));
+dragElement(document.getElementById("piece3"));
+dragElement(document.getElementById("piece4"));
+dragElement(document.getElementById("piece5"));
 
 function dragElement(elmnt) {
-  let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    elmnt.onmousedown = dragMouseDown;
-  
+  let pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  elmnt.onmousedown = dragMouseDown;
 
   function dragMouseDown(e) {
     e = e || window.event;
@@ -84,8 +85,8 @@ function dragElement(elmnt) {
     pos3 = e.clientX;
     pos4 = e.clientY;
     // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
   }
 
   function closeDragElement() {
@@ -94,5 +95,3 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 }
-
-
